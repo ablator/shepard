afff09d5c8033d5a0ccd421ee8c3540968e6756b
Daniel Jilg <winsmith@winsmith.de>
2017-09-03 19:19:11 +0200

    Server connection

--- shepard/shepard/shepard.swift
+++ shepard/shepard/shepard.swift
@@ -1,6 +1,6 @@
 //
 //  shepard.swift
-//  shepard is a client for the ablaator functionality switching server.
+//  shepard is a client for the ablator functionality switching server.
 //  For more info, see http://github.com/ablator
 //
 //  Created by Daniel Jilg on 03.09.17.
@@ -13,7 +13,7 @@ public class AblatorClient {
     // MARK: - Properties
     
     private static var defaultAblatorClient: AblatorClient = {
-        let ablatorClient = AblatorClient(baseURL: URL(string: "http://ablator.space")!)
+        let ablatorClient = AblatorClient(baseURL: string: "http://ablator.space/")
         
         // Configuration
         // ...
@@ -23,11 +23,11 @@ public class AblatorClient {
     
     // MARK: -
     
-    let baseURL: URL
+    let baseURL: String
     
     // Initialization
     
-    private init(baseURL: URL) {
+    private init(baseURL: String) {
         self.baseURL = baseURL
     }
     
@@ -36,5 +36,34 @@ public class AblatorClient {
     class func `default`() -> AblatorClient {
         return defaultAblatorClient
     }
-
+    
+    // MARK: - Caching
+    
+    func cachedFunctionalityFor(user: String, functionality: String) -> String? {
+        // caching is not implemented yet
+        return nil
+    }
+    
+    // MARK: - Server Connections
+    
+    func urlForMethod(method: String, user: String, functionalityID: String) -> URL? {
+        let urlString = "\(self.baseURL)api/v1/\(method)/\(user)/\(functionalityID)/"
+        return URL(string: urlString)
+    }
+    
+    func updateFunctionalityCacheFor(user: String, functionalityID: String, completed: @escaping ()->()) {
+        let url = urlForMethod(method: "caniuse", user: user, functionalityID: functionalityID)
+        if let usableUrl = url {
+            let request = URLRequest(url: usableUrl)
+            let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let stringData = String(data: data, encoding: String.Encoding.utf8) {
+                        print(stringData) //JSONSerialization
+                    }
+                }
+                completed()
+            })
+            task.resume()
+        }
+    }
 }
